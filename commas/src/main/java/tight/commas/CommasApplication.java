0011b711353230d8a64a61e04e4e3697b1647d94
wminsoo1 <wminsoo1@naver.com>
2024-03-16 20:20:26 +0900

    Initial commit

---
+++ commas/src/main/java/tight/commas/CommasApplication.java
@@ -0,0 +1,13 @@
+package tight.commas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommasApplication.class, args);
+	}
+
+}
---
+++ commas/src/test/java/tight/commas/CommasApplicationTests.java
@@ -0,0 +1,13 @@
+package tight.commas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
