995f3517b27ab36cc9bb549a4f59c8ba1efb787a
wminsoo1 <wminsoo1@naver.com>
2024-03-28 16:28:50 +0900

    feat: parkRepository 생성 및 기능 추가

---
+++ commas/src/main/java/tight/commas/domain/park/repository/ParkRepository.java
@@ -0,0 +1,10 @@
+package tight.commas.domain.park.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tight.commas.domain.park.entity.Park;
+
+@Repository
+public interface ParkRepository extends JpaRepository<Park, Long> {
+    Park findByParkName(String parkName);
+}
