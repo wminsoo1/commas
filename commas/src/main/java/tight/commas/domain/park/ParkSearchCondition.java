b21325c7c53fad61e6995f2164c95d1caa9a61c7
wminsoo1 <wminsoo1@naver.com>
2024-04-07 15:10:24 +0900

    feat: ParkSearchCondition tag 리스트 추가

--- commas/src/main/java/tight/commas/domain/park/ParkSearchCondition.java
+++ commas/src/main/java/tight/commas/domain/park/ParkSearchCondition.java
@@ -1,8 +1,13 @@
 package tight.commas.domain.park;
 
 import lombok.Data;
+import tight.commas.domain.review.Tag;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class ParkSearchCondition {
     private String parkName;
+    private List<Tag> tags = new ArrayList<>();
 }
