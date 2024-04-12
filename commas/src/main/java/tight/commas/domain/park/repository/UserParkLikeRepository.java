bb8c044ae624abb00e83e9c4082837ce92c88ab8
wminsoo1 <wminsoo1@naver.com>
2024-04-13 03:27:11 +0900

    feat: UserParkLikeRepository 메서드 추가

--- commas/src/main/java/tight/commas/domain/park/repository/UserParkLikeRepository.java
+++ commas/src/main/java/tight/commas/domain/park/repository/UserParkLikeRepository.java
@@ -1,6 +1,7 @@
 package tight.commas.domain.park.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import tight.commas.domain.park.entity.Park;
 import tight.commas.domain.park.entity.UserParkLike;
 import tight.commas.domain.user.entity.User;
@@ -13,4 +14,7 @@ public interface UserParkLikeRepository extends JpaRepository<UserParkLike, Long
     Optional<UserParkLike> findAllByUserAndPark(User user, Park park);
 
     List<UserParkLike> findAllByUserId(Long userId);
+
+    @Query("SELECT upl FROM UserParkLike upl WHERE upl.user.id = :userId")
+    List<UserParkLike> findByUserId(Long userId);
 }
