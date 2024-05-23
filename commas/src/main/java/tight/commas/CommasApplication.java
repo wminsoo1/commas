d920b91065f109d262b2b1355637e5c06778ac4d
wminsoo1 <wminsoo1@naver.com>
2024-05-23 19:42:24 +0900

    feat: 에노테이션 추가

--- commas/src/main/java/tight/commas/CommasApplication.java
+++ commas/src/main/java/tight/commas/CommasApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @EnableJpaAuditing
+@EnableJpaRepositories
 @SpringBootApplication
 public class CommasApplication {
 
